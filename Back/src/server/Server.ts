b74b21b7d4aafcf6382108aef0d055fe5b0f1024
Matheus Andrell <matheusandrell27@gmail.com>
2023-12-10 13:24:21 -0300

    Cadastro1 Backend

--- Back/src/server/Server.ts
+++ Back/src/server/Server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import './shared/services/TranslateYup';
-import {router } from './routes/usuariosRoutes'
+import { router } from './routes/usuariosRoutes'
 
 const server = express();
 server.use(express.json());
+const cors = require('cors');
+const app = server;
+
+app.use(cors());
 
 server.use(router);
 
-export {server};
+export { server };
